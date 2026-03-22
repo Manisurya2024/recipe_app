158486ec9c1991e5f41907ffa0090069d07f5e1a
Mani <mani@example.com>
2026-03-21 23:40:02 -0400

    final full fix

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,25 +1,21 @@
 class RecipesController < ApplicationController
- before_action :require_login, except: [:index, :show], unless: -> { request.format.json? }
+  before_action :require_login, except: [:index, :show], unless: -> { request.format.json? }
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
-  before_action :authorize_recipe_owner, only: [:edit, :update, :destroy]
 
   def index
     @recipes = Recipe.all
 
-  respond_to do |format|
-    format.html   # for browser UI
-    format.json { render json: @recipes }
+    respond_to do |format|
+      format.html
+      format.json { render json: @recipes }
+    end
   end
-  end
-  
 
   def show
-      @recipe = Recipe.find(params[:id])
-
-  respond_to do |format|
-    format.html
-    format.json { render json: @recipe }
-  end
+    respond_to do |format|
+      format.html
+      format.json { render json: @recipe }
+    end
   end
 
   def new
@@ -27,17 +23,16 @@ class RecipesController < ApplicationController
   end
 
   def create
-  @recipe = Recipe.new(recipe_params)
-  @recipe.user = current_user if current_user
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user if current_user
 
-  if @recipe.save
-    redirect_to @recipe, notice: "Recipe created successfully."
-  else
-    # 👇 THIS WILL SHOW ERROR
-    flash.now[:alert] = @recipe.errors.full_messages.join(", ")
-    render :new
+    if @recipe.save
+      redirect_to @recipe, notice: "Recipe created successfully."
+    else
+      flash.now[:alert] = @recipe.errors.full_messages.join(", ")
+      render :new
+    end
   end
-end
 
   def edit
   end
@@ -46,13 +41,13 @@ end
     if @recipe.update(recipe_params)
       redirect_to @recipe, notice: "Recipe updated successfully."
     else
-      render :edit, status: :unprocessable_entity
+      render :edit
     end
   end
 
   def destroy
     @recipe.destroy
-    redirect_to recipes_path, notice: "Recipe deleted successfully."
+    redirect_to recipes_path
   end
 
   private
@@ -61,11 +56,7 @@ end
     @recipe = Recipe.find(params[:id])
   end
 
-  def authorize_recipe_owner
-    redirect_to recipes_path, alert: "Not authorized." unless @recipe.user == current_user
-  end
-
   def recipe_params
-  params.require(:recipe).permit(:title, :description, :ingredients, :instructions)
-end
+    params.require(:recipe).permit(:title, :description, :ingredients, :instructions)
+  end
 end
