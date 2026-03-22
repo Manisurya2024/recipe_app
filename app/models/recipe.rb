ccc66c240d3c33a5963d84ce407338350562f8ba
Mani <mani@example.com>
2026-03-21 21:47:39 -0400

    fix category optional

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  belongs_to :category
+  belongs_to :category, optional: true
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true
