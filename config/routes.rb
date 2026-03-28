1a0d6d219a0e3fc399f9719c0c2ce4e0a843a6c0
Mani <mani@example.com>
2026-03-28 19:00:06 -0400

    fix comment delete route

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy", as: :logout
+  delete "/logout", to: "sessions#destroy"
 
   resources :users, only: [:show]
   resources :categories
+
   resources :recipes do
     resources :comments, only: [:create, :destroy]
   end
