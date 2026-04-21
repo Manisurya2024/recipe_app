10257bb1d565f711fd76468dec134497c6715a42
Mani <mani@example.com>
2026-04-21 00:37:17 -0400

    fix api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   root "api/v1/recipes#index"
 
-  get "/signup", to: "users#new"
-  post "/signup", to: "users#create"
-
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-
-  delete "/logout", to: "sessions#destroy"
-
-  resources :categories
-  resources :recipes   # ✅ ADD THIS
+  namespace :api do
+    namespace :v1 do
+      resources :recipes
+    end
+  end
 end
